26fc1fe70f21773788306e17e174d35a2bd14915
MRVov <38990500+MRVov@users.noreply.github.com>
2018-05-05 19:29:01 +0300

    Add files via upload

---
+++ frontend/src/lib/Company.js
@@ -0,0 +1,32 @@
+// import * as angular from "angular";
+// import IService = restangular.IService;
+// import * as restangular from "restangular";
+/**
+ * Created by user on 05.01.17.
+ */
+
+angular.module('app').service('Company', ['Restangular', Company]);
+
+function Company(Restangular){
+  // public current: {};
+  // public items: Array<any>;
+    var current = {},
+    items = [];
+    var service={
+        current:current,
+        items:items,
+
+        selectCurrent:selectCurrent
+    };
+    return service;
+
+    function selectCurrent(id) {
+        Restangular.one('api/v1/companies/', id).get().then((res)=>service.current = res);
+    }
+
+
+};
+
+
+
+
